85dc4e2dbff194919a4672a4aa561309f92748bb
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2018-05-08 19:36:15 -0300

    Criando activity de detalhes de amigos.

---
+++ app/src/main/java/br/ufg/pos/fswmd/myfriendsapp/detailFriend/activity/DetailFriendActivity.kt
@@ -0,0 +1,13 @@
+package br.ufg.pos.fswmd.myfriendsapp.detailFriend.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import br.ufg.pos.fswmd.myfriendsapp.R
+
+class DetailFriendActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_friend)
+    }
+}
