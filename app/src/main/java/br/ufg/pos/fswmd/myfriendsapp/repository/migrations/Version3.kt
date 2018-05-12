0269b8907730d95d4136ff2b315794c0d217a261
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2018-05-12 10:34:41 -0300

    Transferindo constantes para as classes específicas de migrações.

--- app/src/main/java/br/ufg/pos/fswmd/myfriendsapp/repository/migrations/Version3.kt
+++ app/src/main/java/br/ufg/pos/fswmd/myfriendsapp/repository/migrations/Version3.kt
@@ -1,11 +1,14 @@
 package br.ufg.pos.fswmd.myfriendsapp.repository.migrations
 
 import android.database.sqlite.SQLiteDatabase
-import br.ufg.pos.fswmd.myfriendsapp.repository.QUERY_ADD_PHOTO_URL_COLUMN_TABLE_FRIEND
+import br.ufg.pos.fswmd.myfriendsapp.repository.FRIEND_TABLE_NAME
+import br.ufg.pos.fswmd.myfriendsapp.repository.KEY_PHOTO_URL
 
 class Version3(
         private val db: SQLiteDatabase
     ) : AbstractMigrateVersionChain(3) {
+
+    private val QUERY_ADD_PHOTO_URL_COLUMN_TABLE_FRIEND = "ALTER TABLE $FRIEND_TABLE_NAME ADD COLUMN $KEY_PHOTO_URL TEXT"
 
     override fun getNext(): MigrateVersion? {
         return null
